6959c391cc3ed2ec34ca68176323ff34b9de45f1
Ralf Karrenberg <karrenberg@cs.uni-saarland.de>
2010-07-26 08:13:15 +0200

    added Packetizer/api.h to include folder
    - modified env setup scripts accordingly

--- setupenv.sh
+++ setupenv.sh
@@ -1,6 +1,6 @@
 #home
-#export PACKETIZER_INSTALL_DIR=/home/ralf/Projekte/proj_anysl/install/build-gcc-opt/packetizer
-#export LLVM_INSTALL_DIR=/home/ralf/Projekte/proj_anysl/install/build-gcc-opt/llvm
+#export PACKETIZER_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-opt/packetizer
+#export LLVM_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-opt/llvm
 #uni
 export PACKETIZER_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-opt/packetizer
 export LLVM_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-opt/llvm
@@ -8,7 +8,6 @@ export LLVM_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-opt/llvm
 export PATH=$PATH:./include
 export PATH=$PATH:$LLVM_INSTALL_DIR/bin
 export PATH=$PATH:$LLVM_INSTALL_DIR/include
-export PATH=$PATH:$PACKETIZER_INSTALL_DIR/include
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/lib
--- setupenvdebug.sh
+++ setupenvdebug.sh
@@ -1,14 +1,13 @@
 #home
-export PACKETIZER_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-debug/packetizer
-export LLVM_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-debug/llvm
+#export PACKETIZER_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-debug/packetizer
+#export LLVM_INSTALL_DIR=/home/ralf/proj/anysl/install/build-gcc-debug/llvm
 #uni
-#export PACKETIZER_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-debug/packetizer
-#export LLVM_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-debug/llvm
+export PACKETIZER_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-debug/packetizer
+export LLVM_INSTALL_DIR=/local/karrenberg/proj/anysl/install/build-gcc-debug/llvm
 
 export PATH=$PATH:./include
 export PATH=$PATH:$LLVM_INSTALL_DIR/bin
 export PATH=$PATH:$LLVM_INSTALL_DIR/include
-export PATH=$PATH:$PACKETIZER_INSTALL_DIR/include
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/lib
