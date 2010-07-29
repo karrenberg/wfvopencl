7bf728070cbc14c4747bf4495074afe7e30a9a37
Ralf Karrenberg <karrenberg@cs.uni-saarland.de>
2010-07-29 17:07:45 +0200

    renamed kernel name of TestLoopBarrier

--- test/TestLoopBarrier/TestLoopBarrier.cpp
+++ test/TestLoopBarrier/TestLoopBarrier.cpp
@@ -167,7 +167,7 @@ int main(int argc, char** argv)
 
     // Create the compute kernel in the program we wish to run
     //
-    kernel = clCreateKernel(program, "square", &err);
+    kernel = clCreateKernel(program, "TestLoopBarrier", &err);
     if (!kernel || err != CL_SUCCESS)
     {
         printf("Error: Failed to create compute kernel!\n");
