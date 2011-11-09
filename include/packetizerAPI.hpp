a994f7bccee2c7252d0ac7d27b2ff3ab652cda05
Thomas Schaub <t.schaub@gmx.de>
2011-11-09 13:31:32 +0100

    changed license to match LLVM (BSD-style)

--- include/packetizerAPI.hpp
+++ include/packetizerAPI.hpp
@@ -3,23 +3,10 @@
  * @date   10.12.2008
  * @author Ralf Karrenberg
  *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See the COPYING file in the root directory for details.
  *
  * Copyright (C) 2008, 2009, 2010, 2011 Saarland University
- *
- * This file is part of Packetizer.
- *
- * Packetizer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Packetizer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Packetizer.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #ifndef _PACKETIZERAPI_HPP
--- src/llvmTools.hpp
+++ src/llvmTools.hpp
@@ -3,23 +3,10 @@
  * @date   27.05.2010
  * @author Ralf Karrenberg
  *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See the COPYING file in the root directory for details.
  *
- * Copyright (C) 2010 Saarland University
- *
- * This file is part of packetizedOpenCL.
- *
- * packetizedOpenCL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * packetizedOpenCL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with packetizedOpenCL.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2008, 2009, 2010, 2011 Saarland University
  *
  */
 #ifndef _LLVMTOOLS_H
